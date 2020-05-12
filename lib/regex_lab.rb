b0659253e7538a97e8e8463f84f525a9f3a480cb
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-12 07:25:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,11 +13,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\w{5}/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+  text.scan(/\b\b/)
 end
 
 def valid_phone_number?(phone)
