3de366c5cfb7d726a1021886cc7ce541d6e46303
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-12 06:59:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
 def starts_with_a_vowel?(word)
-
+  
+  first_char= word[0]
+  if (first_char.match(/[aeiouAEIOU]/))
+    return true 
+  else 
+    return false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  arr_of_ending_with_ing=text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
-
+  numbers=phone.scan(/\d/)
+  if (numbers.length == 10 )
+    return true
+  else 
+    return false
+  end 
 end
